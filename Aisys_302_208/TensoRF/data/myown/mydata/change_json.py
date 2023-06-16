442052e880121cb8d1d825d77455f9d589c3b818
HykyungYoon <yhk04150@gmail.com>
2023-06-16 13:50:20 +0900

    Aisys_302_208

---
+++ Aisys_302_208/TensoRF/data/myown/mydata/change_json.py
@@ -0,0 +1,21 @@
+import os, sys
+import json
+
+paths = os.getcwd()
+files = 'transforms_test_jpg.json'
+
+with open(os.path.join(paths, files), 'r') as f:
+  lines = json.load(f)
+  number_of_info = []
+  frames = lines['frames']
+  for frame in frames:
+    frame_path = frame['file_path']
+    print(frame_path[2:-4])
+    number_of_info.append(frame_path)
+    frame['file_path'] = frame_path[2:-4]
+  print(lines)
+
+  with open(os.path.join(paths, 'transforms_test.json'), 'w') as file:
+    json.dump(lines, file,indent="\t")
+    
+print(f'length pf frame_info :{len(number_of_info)}')
